6253540a5a320b6368c742c0847b24c725a943cc
Visar <visarsulejmani2@gmail.com>
2021-09-22 20:00:19 +0200

    modified

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
-<?php 
-    echo "hello <br>";
-    $name = "John";
-    $lname= "has a submarine";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
 
-    echo $name . $lname;
+    $open_paragraph='<p>';
+    $open_next='this is it,';
+    $openn =' this is a paragraph';
+  
+    echo $open_paragraph . $open_next .'another text' . $openn;
 
-
-
-
-?>
+    ?>
+</body>
+</html>
